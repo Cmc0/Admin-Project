e9c3315788e38adac3fba29703c650867965fef1
cmc6 <dimlt@vip.qq.com>
2022-05-24 17:47:05 +0800

    [v0.0.0] [update]

---
+++ ProjectUtil/src/main/java/com/cmc/projectutil/ProjectutilApplication.java
@@ -0,0 +1,13 @@
+package com.cmc.projectutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectutilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectutilApplication.class, args);
+    }
+
+}
---
+++ ProjectUtil/src/test/java/com/cmc/projectutil/ProjectutilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmc.projectutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectutilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
