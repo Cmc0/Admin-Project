a740930671c582f0e5cc38eae32d1b4ccd1153e1
cmc6 <dimlt@vip.qq.com>
2022-05-24 17:49:33 +0800

    [v0.0.0] [update]

--- ProjectUtil/src/main/java/com/cmc/projectutil/ProjectUtilApplication.java
+++ ProjectUtil/src/main/java/com/cmc/projectutil/ProjectUtilApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectutilApplication {
+public class ProjectUtilApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProjectutilApplication.class, args);
+        SpringApplication.run(ProjectUtilApplication.class, args);
     }
 
 }
