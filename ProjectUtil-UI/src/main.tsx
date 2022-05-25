a2cc69f1ddee598819f205f0a613535bcce8c67a
cmc6 <dimlt@vip.qq.com>
2022-05-25 10:14:25 +0800

    [v0.0.0] [update]

--- ProjectUtil-UI/src/main.tsx
+++ ProjectUtil-UI/src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import zhCN from 'antd/lib/locale/zh_CN'
+import {ConfigProvider} from 'antd'
+import './index.less'
+import "antd/dist/antd.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ConfigProvider locale={zhCN}>
+            <App/>
+        </ConfigProvider>
+    </React.StrictMode>
 )
--- ProjectUtil-UI/vite.config.ts
+++ ProjectUtil-UI/vite.config.ts
@@ -1,7 +1,31 @@
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
+import {resolve} from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default ({mode}) =>
+    defineConfig({
+        plugins: [react()],
+        resolve: {
+            alias: {
+                '@': resolve(__dirname, 'src'),
+            },
+        },
+        server: {
+            port: 7111,
+            proxy: {
+                '/api': {
+                    target: loadEnv(mode, process.cwd()).VITE_API_BASE_URL,
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, ''),
+                },
+            },
+        },
+        css: {
+            preprocessorOptions: {
+                less: {
+                    javascriptEnabled: true,
+                },
+            },
+        },
+    })
