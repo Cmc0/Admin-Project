35d0990827c8cdb1783c76f9c2c08e6f405c1ed5
cmc6 <dimlt@vip.qq.com>
2022-05-25 10:46:31 +0800

    [v0.0.0] [update]

--- ProjectUtil-UI/src/main.tsx
+++ ProjectUtil-UI/src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import zhCN from 'antd/lib/locale/zh_CN'
-import {ConfigProvider} from 'antd'
 import './index.less'
+
+import {ConfigProvider} from 'antd'
+import zhCN from 'antd/lib/locale/zh_CN'
 import "antd/dist/antd.css"
+
+import moment from 'moment';
+import 'moment/dist/locale/zh-cn';
+
+moment.locale('zh-cn');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
