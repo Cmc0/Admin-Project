79a79359a20729a1548ed7e3f5c266b017d187fa
法外狂徒李四 <dimensional_logic@qq.com>
2022-05-26 22:46:37 +0800

    [v0.0.0] [update]

--- ProjectUtil/src/main/java/com/cmc/projectutil/configuration/MybatisPlusConfiguration.java
+++ ProjectUtil/src/main/java/com/cmc/projectutil/configuration/MybatisPlusConfiguration.java
@@ -2,6 +2,7 @@ package com.cmc.projectutil.configuration;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +21,8 @@ public class MybatisPlusConfiguration {
         // 分页插件
         PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
         mybatisPlusInterceptor.addInnerInterceptor(paginationInnerInterceptor);
+        // 乐观锁
+        mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
 
         log.info("MybatisPlus 插件启动");
         return mybatisPlusInterceptor;
---
+++ ProjectUtil/src/main/java/com/cmc/projectutil/model/entity/BaseEntity.java
@@ -0,0 +1,37 @@
+package com.cmc.projectutil.model.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.Version;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@ApiModel(description = "实体类基类")
+public class BaseEntity {
+
+    @TableField(fill = FieldFill.INSERT)
+    @ApiModelProperty(value = "创建人id")
+    private Long createId;
+
+    @TableField(fill = FieldFill.INSERT)
+    @ApiModelProperty(value = "创建时间")
+    private Date createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @ApiModelProperty(value = "修改人id")
+    private Long updateId;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @ApiModelProperty(value = "修改时间")
+    private Date updateTime;
+
+    @Version
+    @TableField(fill = FieldFill.INSERT)
+    @ApiModelProperty(value = "乐观锁")
+    private Integer version;
+
+}
