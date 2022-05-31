ba589631cf66d25893967e057f497447484609b9
法外狂徒李四 <dimensional_logic@qq.com>
2022-05-31 23:42:44 +0800

    【v0.0.0】【update】

--- ProjectUtil-UI/src/componse/Jump/Jump.tsx
+++ ProjectUtil-UI/src/componse/Jump/Jump.tsx
@@ -2,5 +2,5 @@ import {Navigate} from "react-router-dom";
 import {MainLayoutRouterPathList} from "@/router/MainLayoutRouterList";
 
 export default function () {
-    return <Navigate to={MainLayoutRouterPathList[0]}/>
+    return <Navigate to={MainLayoutRouterPathList[0]!}/>
 }
