fd9fef5c034d786336abf7fcad0b44c60eb1cf76
法外狂徒李四 <dimensional_logic@qq.com>
2022-06-02 23:57:06 +0800

    【v0.0.3】【update】

--- Helper-App/src/page/MyConvert/MyConvert.tsx
+++ Helper-App/src/page/MyConvert/MyConvert.tsx
@@ -1,19 +1,24 @@
 import {Button, Drawer, Dropdown, Form, Input, Menu, PageHeader, Space, Typography} from "antd";
 import {useState} from "react";
+import {ToastError, ToastSuccess} from "@/util/ToastUtil";
 
 interface IFunctionButton {
     name: string // 按钮名称
     functionStr: string // 按钮执行的方法
 }
 
+const defaultFunctionButton = {
+    name: '测试', functionStr: 'setResult(source)'
+}
+
 export default function () {
 
     const [fbList, setFbList] = useState<IFunctionButton[]>([]); // 方法按钮集合
-    const [source, setSource] = useState<string>('123'); // 要转换的内容
-    const [result, setResult] = useState<string>('465'); // 转换后的内容
+    const [source, setSource] = useState<string>(''); // 要转换的内容
+    const [result, setResult] = useState<string>(''); // 转换后的内容
     const [drawerTitle, setDrawerTitle] = useState<string>(''); // drawer的 title
     const [drawerVisible, setDrawerVisible] = useState<boolean>(false); // drawer的 visible
-    const [drawerForm, setDrawerForm] = useState<IFunctionButton>({name: '测试', functionStr: 'setResult(source)'}); // drawer的 form
+    const [drawerForm, setDrawerForm] = useState<IFunctionButton>(defaultFunctionButton); // drawer的 form
     const [drawerUseForm] = Form.useForm(); // drawer的 useForm
 
     return <div className={"bc vwh100 flex-c"}>
@@ -36,29 +41,39 @@ export default function () {
                 }}/>
             </div>
 
-            <Space direction={"vertical"} className={"m-l-r-20"}>
+            <Space align={"center"} direction={"vertical"} className={"m-l-r-20"}>
                 <>
                     {
                         fbList?.map((item, index) => (
                                 <Dropdown.Button
+                                    size={"small"}
                                     key={index}
                                     overlay={<Menu
                                         onClick={(e) => {
-                                            console.log(e.key)
                                             if (e.key === 'delFunction') {
                                                 fbList.splice(index, 1)
-                                                setFbList(fbList)
+                                                setFbList(fbList.concat())
                                             }
                                         }}
                                         items={[
                                             {
+                                                key: 'editFunction',
+                                                label: `编辑【${item.name}】`,
+                                            },
+                                            {
                                                 key: 'delFunction',
-                                                label: '删除方法',
+                                                label: `删除【${item.name}】`,
                                             },
                                         ]}
                                     />}
                                     onClick={() => {
-                                        new Function("source", "setResult", item.functionStr)(source, setResult)
+                                        try {
+                                            new Function("source", "setResult", item.functionStr)(source, setResult)
+                                            ToastSuccess("操作成功 (*^▽^*)")
+                                        } catch (e) {
+                                            console.error(e)
+                                            ToastError('操作失败 o(╥﹏╥)o')
+                                        }
                                     }}
                                 >{item.name}</Dropdown.Button>
                             )
