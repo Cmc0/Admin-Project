7086ed92e3ec247be7246c8477d04082719aae1f
cmc6 <dimlt@vip.qq.com>
2022-06-06 15:23:16 +0800

    【v0.0.3】【update】

--- Helper-App/vite.config.ts
+++ Helper-App/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import {resolve} from 'path'
 
 // https://vitejs.dev/config/
-export default ({mode}) =>
+export default () =>
     defineConfig({
         plugins: [react()],
         resolve: {
