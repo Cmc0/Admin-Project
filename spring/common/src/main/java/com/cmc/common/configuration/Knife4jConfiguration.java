831a87fb94221f355b7a1e07c6dc1231d40e1ffa
cmc6 <dimlt@vip.qq.com>
2022-06-07 17:46:31 +0800

    【v0.0.3】【update】

--- spring/common/src/main/java/com/cmc/common/configuration/Knife4jConfiguration.java
+++ spring/common/src/main/java/com/cmc/common/configuration/Knife4jConfiguration.java
@@ -2,6 +2,7 @@ package com.cmc.common.configuration;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -16,9 +17,19 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class Knife4jConfiguration {
 
+    @Value("${spring.profiles.active:prod}")
+    private String profile;
+
     @Bean
     public Docket createRestApi() {
-        return new Docket(DocumentationType.OAS_30).apiInfo(new ApiInfoBuilder().build()).select()
+
+        boolean enable = !"prod".equals(profile); // 不是生产环境，才启动swagger
+
+        if (enable) {
+            log.info("Swagger 启动");
+        }
+
+        return new Docket(DocumentationType.OAS_30).enable(enable).apiInfo(new ApiInfoBuilder().build()).select()
             .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)) // 方法需要有ApiOperation注解才能生成接口文档
             .paths(PathSelectors.any()).build();
     }
