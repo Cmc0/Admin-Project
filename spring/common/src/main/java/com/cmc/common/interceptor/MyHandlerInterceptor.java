3a0d6cb9e42a2b299ef92a6ffec9a56752b64f44
cmc6 <dimlt@vip.qq.com>
2022-06-08 14:54:19 +0800

    【v0.0.3】【update】

--- spring/common/src/main/java/com/cmc/common/interceptor/MyHandlerInterceptor.java
+++ spring/common/src/main/java/com/cmc/common/interceptor/MyHandlerInterceptor.java
@@ -1,12 +1,12 @@
 package com.cmc.common.interceptor;
 
-import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.io.IoUtil;
 import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.toolkit.ChainWrappers;
+import com.cmc.common.mapper.UserIdMapper;
 import com.cmc.common.model.constant.BaseConstant;
 import com.cmc.common.model.entity.ParamDO;
 import com.cmc.common.model.entity.UserIdDO;
-import com.cmc.common.service.UserIdService;
 import com.cmc.common.util.MyJwtUtil;
 import com.cmc.common.util.ParamUtil;
 import lombok.SneakyThrows;
@@ -17,7 +17,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.nio.charset.Charset;
 import java.util.List;
 
 /**
@@ -28,7 +27,7 @@ import java.util.List;
 public class MyHandlerInterceptor implements HandlerInterceptor {
 
     @Resource
-    UserIdService userIdService;
+    UserIdMapper userIdMapper;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
@@ -48,9 +47,9 @@ public class MyHandlerInterceptor implements HandlerInterceptor {
     private void handleUserMutuallyExclusive(HttpServletRequest request) {
 
         // 获取请求body
-        String body = IoUtil.read(request.getInputStream(), Charset.defaultCharset());
+        String bodyStr = IoUtil.readUtf8(request.getInputStream());
 
-        ParamDO paramDO = BeanUtil.toBean(JSONUtil.parseObj(body), ParamDO.class);
+        ParamDO paramDO = JSONUtil.toBean(bodyStr, ParamDO.class);
 
         if (!BaseConstant.USER_MUTUALLY_EXCLUSIVE_ID.equals(paramDO.getId())) {
             return;
@@ -71,13 +70,13 @@ public class MyHandlerInterceptor implements HandlerInterceptor {
         String value = paramDO.getValue();
         if ("2".equals(value)) {
             // 2 相同端的互斥（H5/移动端/桌面程序）
-            List<UserIdDO> userIdDbList = userIdService.lambdaQuery().select(UserIdDO::getId).list();
+            List<UserIdDO> userIdDbList = ChainWrappers.lambdaQueryChain(userIdMapper).select(UserIdDO::getId).list();
             for (UserIdDO item : userIdDbList) {
                 MyJwtUtil.removeJwtHashByRequestCategoryOrJwtHash(item.getId(), null, null, false);
             }
         } else if ("3".equals(value)) {
             // 3 所有端都互斥
-            List<UserIdDO> userIdDbList = userIdService.lambdaQuery().select(UserIdDO::getId).list();
+            List<UserIdDO> userIdDbList = ChainWrappers.lambdaQueryChain(userIdMapper).select(UserIdDO::getId).list();
             for (UserIdDO item : userIdDbList) {
                 MyJwtUtil.removeJwtHashByRequestCategoryOrJwtHash(item.getId(), null, null, true);
             }
