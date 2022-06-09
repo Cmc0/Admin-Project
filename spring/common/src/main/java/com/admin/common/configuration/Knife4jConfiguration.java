3b97ccb0abfcee13c0f1080ea03d5eb8dc88792c
cmc6 <dimlt@vip.qq.com>
2022-06-09 15:44:32 +0800

    【v0.0.3】【update】

--- spring/common/src/main/java/com/admin/common/configuration/Knife4jConfiguration.java
+++ spring/common/src/main/java/com/admin/common/configuration/Knife4jConfiguration.java
@@ -4,7 +4,6 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -23,7 +22,7 @@ public class Knife4jConfiguration {
 
         boolean enable = !"prod".equals(profile); // 不是生产环境，才启动swagger
 
-        return new Docket(DocumentationType.OAS_30).enable(enable).apiInfo(new ApiInfoBuilder().build()).select()
+        return new Docket(DocumentationType.OAS_30).enable(enable).select()
             .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)) // 方法需要有ApiOperation注解才能生成接口文档
             .paths(PathSelectors.any()).build();
     }
