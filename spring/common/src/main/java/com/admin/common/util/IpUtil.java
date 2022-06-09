ea8bca66fafa5788498406539efba44b5d5121fc
cmc6 <dimlt@vip.qq.com>
2022-06-09 15:48:31 +0800

    【v0.0.3】【update】

--- spring/common/src/main/java/com/admin/common/util/IpUtil.java
+++ spring/common/src/main/java/com/admin/common/util/IpUtil.java
@@ -9,7 +9,6 @@ import org.lionsoul.ip2region.DbSearcher;
 import org.springframework.core.io.ClassPathResource;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.InputStream;
 
 public class IpUtil {
 
@@ -22,10 +21,8 @@ public class IpUtil {
             // 获取 resource下面的文件，备注：需要更新时，直接访问下面的地址，下载文件之后直接覆盖即可
             // https://gitee.com/lionsoul/ip2region/blob/master/data/ip2region.db
             ClassPathResource cpr = new ClassPathResource("/ip2region/ip2region.db");
-            InputStream inputStream = cpr.getInputStream();
             // 数据文件
-            byte[] dbBinStr = IoUtil.readBytes(inputStream);
-            IoUtil.close(inputStream);
+            byte[] dbBinStr = IoUtil.readBytes(cpr.getInputStream());
 
             searcher = new DbSearcher(dbConfig, dbBinStr); // 这里只支持 MEMORY_ALGORITYM 进行静态赋值，因为只有它才是线程安全的，另外两个都不是线程安全的
         } catch (Exception e) {
