de0a1b4052e61c06c3836335de6993c8c4d66a17
cmc6 <dimlt@vip.qq.com>
2022-06-13 17:33:15 +0800

    【v0.0.0】【update】

--- ant-react/util/OpenApiSyncUtil.ts
+++ ant-react/util/OpenApiSyncUtil.ts
@@ -37,11 +37,20 @@ interface IOpenApiTag { // 例如：{ name: "用户-登录", description: "User 
     description: string
 }
 
+type IOpenApiComponentSchemaPropertyFormat = 'byte' | 'int64' | 'date-time' | 'int32'
+type IOpenApiComponentSchemaPropertyType = 'string' | 'integer' | 'boolean' | 'array'
+
 interface IOpenApiComponentSchemaProperty { // 例如：{ AddOrderNoDTO: { title: '', description: '', ... }}
-    type: string // 字段类型：string integer boolean array
+    $ref?: string // $ref 有值时，type 和 format没值
+    type?: IOpenApiComponentSchemaPropertyType // 字段类型
+    format?: IOpenApiComponentSchemaPropertyFormat
     description: string // 字段描述
     example?: boolean // 默认值，目前只有 boolean 有默认值
-    items?: { type: string } // 如果是集合，才有此值
+    items?: {
+        $ref?: string // $ref 有值时，type 和 format没值
+        type?: string
+        format?: IOpenApiComponentSchemaPropertyFormat
+    } // 如果是集合，才有此值
 }
 
 interface IOpenApi {
@@ -54,6 +63,49 @@ interface IOpenApi {
 
 const fs = require('fs')
 const axios = require('axios')
+
+function getInterfaceType(type: string, format: IOpenApiComponentSchemaPropertyFormat, componentProperty: IOpenApiComponentSchemaProperty) {
+
+    if (type === 'integer') {
+        type = 'number'
+    } else if (type === 'string' && format === 'byte') {
+        type = 'boolean'
+    } else if (type === 'array') {
+        if (componentProperty.items && componentProperty.items.type) {
+            type = getInterfaceType(componentProperty.items.type, componentProperty.items.format!, componentProperty) + '[]'
+        }
+    }
+
+    return type
+}
+
+// 写：interface
+function interfaceWrite(componentFullName: string, fileData: string, component: Record<string, IOpenApiComponentSchemaProperty>) {
+
+    const splitList = componentFullName.split('/');
+    const componentName = splitList[splitList.length - 1];
+
+    fileData += `export interface ${componentName} {\n`
+
+    Object.keys(component).forEach(item => {
+
+        let type = 'error'
+        if (component[item].type) {
+            let format = component[item].format!
+            type = getInterfaceType(component[item].type!, format, component[item])
+        } else if (component[item].$ref) {
+            const refSplitList = component[item].$ref!.split('/');
+            const refComponentName = refSplitList[refSplitList.length - 1];
+            type = refComponentName
+        }
+
+        fileData += `    ${item}?: ${type} // ${component[item].description}\n`
+    })
+
+    fileData += "}\n\n"
+
+    return fileData
+}
 
 // 同步 openApi到 api文件夹
 function start() {
@@ -96,6 +148,12 @@ function start() {
             }
 
             pathList.forEach(subItem => {
+
+                if (subItem.responses) {
+                    const responsesName = subItem.responses["200"].content["*/*"].schema.$ref;
+
+                }
+
                 if (!subItem.requestBody) {
                     // TODO：如果没有入参
                     return
@@ -106,27 +164,7 @@ function start() {
                     return
                 }
                 // 如果有入参
-                const splitList = requestBodyName.split('/');
-                const dtoName = splitList[splitList.length - 1];
-
-                fileData += `// ${item.name} ${subItem.summary || '暂无接口描述'}\n`
-                fileData += `export interface ${dtoName} {\n`
-
-                Object.keys(requestBody).forEach(deepNode => {
-                    let type = requestBody[deepNode].type
-                    if (type === 'integer') {
-                        type = 'number'
-                    } else if (type === 'array') {
-                        type = requestBody[deepNode].items!.type
-                        if (type === 'integer') {
-                            type = 'number'
-                        }
-                        type += '[]'
-                    }
-                    fileData += `    ${deepNode}?: ${type} // ${requestBody[deepNode].description}\n`
-                })
-
-                fileData += "}\n\n"
+                fileData = interfaceWrite(requestBodyName, fileData, requestBody);
             })
 
             // 写入文件
