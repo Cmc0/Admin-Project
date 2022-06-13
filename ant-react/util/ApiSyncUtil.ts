3df06353e22c0eec461fd14c9cdd977090fb686c
cmc6 <dimlt@vip.qq.com>
2022-06-13 12:54:26 +0800

    【v0.0.0】【update】

--- ant-react/util/ApiSyncUtil.ts
+++ ant-react/util/ApiSyncUtil.ts
@@ -2,8 +2,52 @@
 import $http from "./HttpUtil";
 import CommonConstant from "@/model/constant/CommonConstant";
 
+interface IOpenApiPathResRequestBody {
+    content: {
+        "application/json": {
+            schema: {
+                $ref: string // 例如：#/components/schemas/AddOrderNoDTO
+            }
+        }
+    }
+}
+
+interface IOpenApiPathRes {
+    tags: IOpenApiTag[]
+    summary: string // 接口描述
+    operationId: string // 入参
+    requestBody: IOpenApiPathResRequestBody
+}
+
+interface IOpenApiPath {
+    post: IOpenApiPathRes
+}
+
+interface IOpenApiTag { // 例如：{ name": "用户-登录", description": "User Login Controller" }
+    name: string
+    description: string
+}
+
+interface IOpenApiComponentSchemaProperty {
+    type: string // 字段类型：string integer boolean array
+    description: string // 字段描述
+    example?: boolean // 默认值，目前只有 boolean 有默认值
+    items?: { type: string } // 如果是集合，才有此值
+}
+
+interface IOpenApi {
+    tags: IOpenApiTag[]
+    paths: IOpenApiPath[]
+    components: {
+        schemas: Record<string, Record<'properties', IOpenApiComponentSchemaProperty>>
+    }
+}
+
 export default function () {
-    $http.get(CommonConstant.OPEN_API_URL).then(({data}) => {
-        console.log(data)
+    $http.get<IOpenApi>(CommonConstant.OPEN_API_URL).then(({data}) => {
+        data.tags.forEach(item => {
+            const controllerName = item.description.replaceAll(' ', '');
+
+        })
     })
 }
