0a8e0f6362f3c064ec6953815801a76d1c0d6ba2
cmc6 <dimlt@vip.qq.com>
2022-06-23 12:09:01 +0800

    【v0.0.3】【update】

--- ant-react/src/page/sysMonitor/OnlineUser/TableColumnList.tsx
+++ ant-react/src/page/sysMonitor/OnlineUser/TableColumnList.tsx
@@ -2,24 +2,35 @@ import {ActionType, ProColumns} from "@ant-design/pro-components";
 import React from "react";
 import {InDev} from "../../../../util/CommonUtil";
 import {SysSysWebSocketPageVO} from "@/api/SysWebSocketController";
+import {handlerRegion} from "../../../../util/StrUtil";
+import {WebSocketTypeDict, YesNoBaseDict} from "../../../../util/DictUtil";
 
 const TableColumnList = (actionRef: React.RefObject<ActionType>): ProColumns<SysSysWebSocketPageVO>[] => [
+    {
+        dataIndex: 'index',
+        valueType: 'index',
+    },
     {title: 'id', dataIndex: 'id'},
     {title: '用户名', dataIndex: 'userName'},
     {title: 'ip', dataIndex: 'ip'},
     {
         title: 'ip区域',
         dataIndex: 'region',
+        renderText: (text) => {
+            return handlerRegion(text)
+        }
     },
     {title: '浏览器', dataIndex: 'browser'},
     {title: '操作系统', dataIndex: 'os'},
     {
         title: '移动端网页',
         dataIndex: 'mobileFlag',
+        valueEnum: YesNoBaseDict,
     },
     {
         title: '状态',
         dataIndex: 'type',
+        valueEnum: WebSocketTypeDict
     },
     {title: '服务器', dataIndex: 'server'},
     {
@@ -29,7 +40,6 @@ const TableColumnList = (actionRef: React.RefObject<ActionType>): ProColumns<Sys
     {title: '连接时间', dataIndex: 'createTime', sorter: true},
     {
         title: '操作',
-        width: 180,
         dataIndex: 'option',
         valueType: 'option',
         render: (dom, entity) => [
--- ant-react/util/DictUtil.ts
+++ ant-react/util/DictUtil.ts
@@ -1,3 +1,14 @@
-export const YesNoDict = new Map();
+import {TWebSocketType} from "@/model/constant/LocalStorageKey";
+import {ProSchemaValueEnumType} from "@ant-design/pro-components";
+
+export const YesNoDict = new Map<any, ProSchemaValueEnumType>();
 YesNoDict.set(true, {text: '是', status: 'success'})
 YesNoDict.set(false, {text: '否', status: 'error'})
+
+export const YesNoBaseDict = new Map<any, ProSchemaValueEnumType>();
+YesNoBaseDict.set(true, {text: '是'})
+YesNoBaseDict.set(false, {text: '否'})
+
+export const WebSocketTypeDict = new Map<TWebSocketType, ProSchemaValueEnumType>();
+WebSocketTypeDict.set('1', {text: '在线', status: 'success'})
+WebSocketTypeDict.set('2', {text: '隐身', status: 'warning'})
