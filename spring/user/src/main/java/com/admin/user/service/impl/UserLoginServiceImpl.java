7444260d6c8a63cfcb7f70b2d49fcf843f92e19c
cmc6 <dimlt@vip.qq.com>
2022-06-27 15:54:38 +0800

    【v0.0.3】【update】

--- spring/user/src/main/java/com/admin/user/service/impl/UserLoginServiceImpl.java
+++ spring/user/src/main/java/com/admin/user/service/impl/UserLoginServiceImpl.java
@@ -77,7 +77,7 @@ public class UserLoginServiceImpl extends ServiceImpl<SysUserMapper, SysUserDO> 
      */
     private String passwordByAccount(UserLoginByPasswordDTO dto) {
 
-        SysUserDO sysUserDO = lambdaQuery().eq(SysUserDO::getEmail, dto.getAccount()).eq(SysUserDO::getEnableFlag, true)
+        SysUserDO sysUserDO = lambdaQuery().eq(SysUserDO::getEmail, dto.getAccount()).eq(SysUserDO::getDelFlag, true)
             .select(SysUserDO::getPassword, BaseEntityThree::getDelFlag, BaseEntityThree::getEnableFlag,
                 SysUserDO::getJwtSecretSuf, BaseEntityTwo::getId).one();
 
