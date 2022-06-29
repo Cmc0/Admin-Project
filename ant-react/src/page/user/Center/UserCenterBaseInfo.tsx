47b0f62072b5d947717a27c9b44beb2614fcc782
cmc6 <dimlt@vip.qq.com>
2022-06-29 16:34:07 +0800

    【v0.0.3】【update】

--- ant-react/src/page/user/Center/UserCenterBaseInfo.tsx
+++ ant-react/src/page/user/Center/UserCenterBaseInfo.tsx
@@ -5,28 +5,38 @@ import {
     SysUserUpdateBaseInfoDTO
 } from "@/api/SysUserController";
 import {ActionType, ProDescriptions} from "@ant-design/pro-components";
-import {EyeOutlined, UploadOutlined} from "@ant-design/icons/lib";
+import {DeleteOutlined, EyeOutlined} from "@ant-design/icons/lib";
 import CommonConstant from "@/model/constant/CommonConstant";
 import {default as React, useRef, useState} from "react";
 import {UploadFile} from "antd/es/upload/interface";
-import {Button, Upload} from "antd";
-import {sysFileUpload} from "@/api/SysFileController";
 import {CheckImageFileSize, CheckImageFileType, GetPublicDownFileUrl} from "../../../../util/FileUtil";
-import {ToastError, ToastSuccess} from "../../../../util/ToastUtil";
-import {RcFile} from "antd/lib/upload";
 import {RequestData} from "@ant-design/pro-descriptions/lib/useFetchData";
 import {useAppDispatch} from "@/store";
+import {Image, Space, Upload} from "antd";
+import {sysFileUpload} from "@/api/SysFileController";
 import {setUserBaseInfo} from "@/store/userSlice";
+import {execConfirm, ToastError, ToastSuccess} from "../../../../util/ToastUtil";
+import MyIcon from "@/componse/MyIcon/MyIcon";
 
 export default function () {
 
     const appDispatch = useAppDispatch();
 
     const [fileList, setFileList] = useState<UploadFile[]>([]);
+    const [fileLoading, setFileLoading] = useState<boolean>(false)
 
     const actionRef = useRef<ActionType>()
 
     const currentForm = useRef<SysUserUpdateBaseInfoDTO>({})
+
+    function doSysUserUpdateBaseInfo(form: SysUserUpdateBaseInfoDTO) {
+        return sysUserUpdateBaseInfo(form).then(res => {
+            ToastSuccess(res.msg)
+            setFileLoading(false)
+            appDispatch(setUserBaseInfo(form))
+            actionRef.current?.reload()
+        })
+    }
 
     return (
         <ProDescriptions<SysUserBaseInfoVO>
@@ -46,26 +56,6 @@ export default function () {
             editable={{
                 onSave: async (key, record, originRow) => {
                     console.log(key, record, originRow);
-                    if (key === 'avatarUrl') {
-                        // @ts-ignore
-                        const file: RcFile = record.avatarUrl.file;
-                        let avatarUrl = ''
-                        if (file) {
-                            const formData = new FormData()
-                            formData.append('file', file)
-                            formData.append('bucketName', 'public')
-                            formData.append('folderName', 'avatar')
-                            await sysFileUpload(formData, {headers: {'Content-Type': 'multipart/form-data'}}).then(res => {
-                                avatarUrl = res.data
-                            })
-                        }
-                        let formTemp = {...currentForm.current, avatarUrl}
-                        await sysUserUpdateBaseInfo(formTemp).then(res => {
-                            ToastSuccess(res.msg)
-                            appDispatch(setUserBaseInfo(formTemp))
-                            actionRef.current?.reload()
-                        })
-                    }
                     return true;
                 },
             }}
@@ -75,47 +65,54 @@ export default function () {
                     title: '头像',
                     dataIndex: 'avatarUrl',
                     valueType: 'image',
-                    fieldProps: {
-                        preview: {
-                            mask: <EyeOutlined title={"预览"}/>
-                        },
-                    },
+                    editable: false,
                     renderText: (text) => {
                         return text ? GetPublicDownFileUrl(text) : CommonConstant.RANDOM_AVATAR_URL
                     },
-                    renderFormItem: () =>
-                        <Upload
-                            accept={CommonConstant.IMAGE_FILE_ACCEPT_TYPE}
-                            fileList={fileList}
-                            maxCount={1}
-                            // customRequest={(options) => {
-                            //     const formData = new FormData()
-                            //     formData.append('file', options.file)
-                            //     formData.append('bucketName', 'public')
-                            //     formData.append('folderName', 'avatar')
-                            //     sysFileUpload(formData, {headers: {'Content-Type': 'multipart/form-data'}}).then(res => {
-                            //         ToastSuccess(res.msg)
-                            //     })
-                            // }}
-                            listType={"picture"}
-                            beforeUpload={() => {
-                                return false // 手动上传
-                            }}
-                            onChange={(info) => {
-                                console.log(info)
-                                if (!CheckImageFileType(info.file.type!)) {
-                                    ToastError("暂不支持此文件类型：" + info.file.type + "，请重新选择")
-                                    return
-                                }
-                                if (!CheckImageFileSize(info.file.size!, 2097152)) {
-                                    ToastError("图片大于 2MB，请重新选择")
-                                    return
-                                }
-                                setFileList(info.fileList)
-                            }}
-                        >
-                            <Button icon={<UploadOutlined/>}>选择文件</Button>
-                        </Upload>
+                    render: (dom: React.ReactNode, entity) => {
+                        return <Space size={16}>
+                            <Image src={dom as string} height={32} preview={{mask: <EyeOutlined title={"预览"}/>}}/>
+                            <Upload
+                                disabled={fileLoading}
+                                accept={CommonConstant.IMAGE_FILE_ACCEPT_TYPE}
+                                fileList={fileList}
+                                maxCount={1}
+                                showUploadList={false}
+                                beforeUpload={(file) => {
+                                    if (!CheckImageFileType(file.type)) {
+                                        ToastError("暂不支持此文件类型：" + file.type + "，请重新选择")
+                                        return false
+                                    }
+                                    if (!CheckImageFileSize(file.size!, 2097152)) {
+                                        ToastError("图片大于 2MB，请重新选择")
+                                        return false
+                                    }
+                                    return true
+                                }}
+                                customRequest={(options) => {
+                                    setFileLoading(true)
+                                    const formData = new FormData()
+                                    formData.append('file', options.file)
+                                    formData.append('bucketName', 'public')
+                                    formData.append('folderName', 'avatar')
+                                    sysFileUpload(formData, {headers: {'Content-Type': 'multipart/form-data'}}).then(res => {
+                                        doSysUserUpdateBaseInfo({...currentForm.current, avatarUrl: res.data})
+                                    })
+                                }}
+                                onChange={(info) => {
+                                    setFileList(info.fileList)
+                                }}
+                            >
+                                <a> <MyIcon title={fileLoading ? '上传中' : '上传头像'}
+                                            icon={fileLoading ? 'LoadingOutlined' : 'UploadOutlined'}/></a>
+                            </Upload>
+                            {entity.avatarUrl && <a onClick={() => {
+                                execConfirm(() => {
+                                    return doSysUserUpdateBaseInfo({...currentForm.current, avatarUrl: ''})
+                                }, undefined, '确定移除头像吗？')
+                            }}><DeleteOutlined title={"移除头像"} className={"red3"}/></a>}
+                        </Space>
+                    }
                 },
                 {
                     title: '昵称',
