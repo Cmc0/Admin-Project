783209da777b70e93e6431575fdbab1f58ffd36d
cmc6 <dimlt@vip.qq.com>
2022-06-29 17:26:00 +0800

    【v0.0.3】【update】

--- ant-react/src/page/user/Center/Center.tsx
+++ ant-react/src/page/user/Center/Center.tsx
@@ -2,22 +2,27 @@ import {SettingOutlined, UserOutlined} from "@ant-design/icons/lib";
 import UserCenterBaseInfo from "@/page/user/Center/UserCenterBaseInfo";
 import UserCenterSetting from "@/page/user/Center/UserCenterSetting";
 import {Card, Tabs} from "antd";
+import {RouteContext, RouteContextType} from "@ant-design/pro-components";
 
 export default function () {
     return (
-        <Card>
-            <Tabs tabPosition='left'>
-                <Tabs.TabPane key={'1'} tab={
-                    <span><UserOutlined/> 个人资料</span>
-                }>
-                    <UserCenterBaseInfo/>
-                </Tabs.TabPane>
-                <Tabs.TabPane key={'2'} tab={
-                    <span><SettingOutlined/> 账号设置</span>
-                }>
-                    <UserCenterSetting/>
-                </Tabs.TabPane>
-            </Tabs>
-        </Card>
+        <RouteContext.Consumer>
+            {(value: RouteContextType) => {
+                return <Card>
+                    <Tabs tabPosition={value.isMobile ? 'top' : 'left'}>
+                        <Tabs.TabPane key={'1'} tab={
+                            <span><UserOutlined/> 个人资料</span>
+                        }>
+                            <UserCenterBaseInfo/>
+                        </Tabs.TabPane>
+                        <Tabs.TabPane key={'2'} tab={
+                            <span><SettingOutlined/> 账号设置</span>
+                        }>
+                            <UserCenterSetting/>
+                        </Tabs.TabPane>
+                    </Tabs>
+                </Card>
+            }}
+        </RouteContext.Consumer>
     )
 }
