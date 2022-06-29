b1e3203658d15da602e36efee0c8b185a85c5310
法外狂徒李四 <dimensional_logic@qq.com>
2022-06-29 20:42:34 +0800

    【v0.0.3】【update】

--- spring/file/src/main/java/com/admin/file/service/impl/SysFileServiceImpl.java
+++ spring/file/src/main/java/com/admin/file/service/impl/SysFileServiceImpl.java
@@ -62,6 +62,11 @@ public class SysFileServiceImpl extends ServiceImpl<SysFileMapper, SysFileDO> im
             ApiResultVO.error("操作失败：文件名不能为空");
         }
 
+        String fileType = sysFileUploadTypeEnum.checkFileType(dto.getFile().getInputStream());
+        if (fileType == null) {
+            ApiResultVO.error("操作失败：暂不支持此文件类型【" + originalFilename + "】，请重新选择");
+        }
+
         // 把文件夹路径合法化
         List<String> splitList = StrUtil.splitTrim(sysFileUploadTypeEnum.getFolderName(), '/');
         if (splitList.size() == 0) {
@@ -72,11 +77,6 @@ public class SysFileServiceImpl extends ServiceImpl<SysFileMapper, SysFileDO> im
 
         // 拼接路径
         String path = userId + "/" + CollUtil.join(splitList, "/") + "/";
-
-        String fileType = sysFileUploadTypeEnum.checkFileType(dto.getFile().getInputStream());
-        if (fileType == null) {
-            ApiResultVO.error("操作失败：暂不支持此文件类型【" + originalFilename + "】，请重新选择");
-        }
 
         // 新的文件名
         String newFileName = IdUtil.simpleUUID() + originalFilename.substring(originalFilename.lastIndexOf("."));
