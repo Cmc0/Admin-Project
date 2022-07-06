2fba3c3dd75294a896705146ca8e5ca0a9535f5c
法外狂徒李四 <dimensional_logic@qq.com>
2022-07-06 19:37:33 +0800

    【v0.0.3】【update】

---
+++ ant-react/src/api/ServerController.ts
@@ -0,0 +1,17 @@
+import {AxiosRequestConfig} from "axios";
+import $http from "../../util/HttpUtil";
+
+export interface ServerWorkInfoVO {
+    cpuUsed?: number // CPU使用率（0-100）
+    diskTotal?: number // 磁盘总量（字节）
+    diskUsable?: number // 磁盘可以使用总量（字节）
+    jvmFreeMemory?: number // JVM中内存剩余大小（字节）
+    jvmTotalMemory?: number // JVM中内存总大小（字节）
+    memoryAvailable?: number // 系统可用内存（字节）
+    memoryTotal?: number // 系统总内存（字节）
+}
+
+// 服务器-管理 服务器运行情况
+export function serverWorkInfo(config?: AxiosRequestConfig) {
+    return $http.myPost<ServerWorkInfoVO>('/server/workInfo', undefined, config)
+}
