a67fca7f2dcb7fe0352a0f833f70db3f1e284844
法外狂徒李四 <dimensional_logic@qq.com>
2022-07-06 22:23:36 +0800

    【v0.0.3】【update】

--- ant-react/src/page/dashboard/Workplace/Workplace.tsx
+++ ant-react/src/page/dashboard/Workplace/Workplace.tsx
@@ -1,3 +1,75 @@
+import {RouteContext, RouteContextType, StatisticCard} from "@ant-design/pro-components";
+import {Statistic} from "antd";
+
 export default function () {
-    return 'Hello World'
+    return (
+        <RouteContext.Consumer>
+            {(value: RouteContextType) => {
+                return <>
+                    <StatisticCard.Group direction={value.isMobile ? 'column' : 'row'}>
+                        <StatisticCard
+                            statistic={{
+                                title: '内存使用',
+                                value: '108MB',
+                                description: <Statistic title="占比" value="61.5%"/>,
+                            }}
+                            chart={
+                                <img
+                                    src="https://gw.alipayobjects.com/zos/alicdn/ShNDpDTik/huan.svg"
+                                    alt="百分比"
+                                    width="100%"
+                                />
+                            }
+                            chartPlacement="left"
+                        />
+                        <StatisticCard
+                            statistic={{
+                                title: '内存使用',
+                                value: '108MB',
+                                description: <Statistic title="占比" value="61.5%"/>,
+                            }}
+                            chart={
+                                <img
+                                    src="https://gw.alipayobjects.com/zos/alicdn/ShNDpDTik/huan.svg"
+                                    alt="百分比"
+                                    width="100%"
+                                />
+                            }
+                            chartPlacement="left"
+                        />
+                        <StatisticCard
+                            statistic={{
+                                title: '内存使用',
+                                value: '108MB',
+                                description: <Statistic title="占比" value="61.5%"/>,
+                            }}
+                            chart={
+                                <img
+                                    src="https://gw.alipayobjects.com/zos/alicdn/ShNDpDTik/huan.svg"
+                                    alt="百分比"
+                                    width="100%"
+                                />
+                            }
+                            chartPlacement="left"
+                        />
+                        <StatisticCard
+                            statistic={{
+                                title: '内存使用',
+                                value: '108MB',
+                                description: <Statistic title="占比" value="61.5%"/>,
+                            }}
+                            chart={
+                                <img
+                                    src="https://gw.alipayobjects.com/zos/alicdn/ShNDpDTik/huan.svg"
+                                    alt="百分比"
+                                    width="100%"
+                                />
+                            }
+                            chartPlacement="left"
+                        />
+                    </StatisticCard.Group>
+                </>
+            }}
+        </RouteContext.Consumer>
+    )
 }
