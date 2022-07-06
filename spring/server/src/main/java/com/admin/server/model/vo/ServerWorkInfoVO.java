3a65cc6a9a74ba0e612fddbb1a37224cb063adf9
cmc6 <dimlt@vip.qq.com>
2022-07-06 17:48:48 +0800

    【v0.0.3】【update】

--- spring/server/src/main/java/com/admin/server/model/vo/ServerWorkInfoVO.java
+++ spring/server/src/main/java/com/admin/server/model/vo/ServerWorkInfoVO.java
@@ -6,25 +6,25 @@ import lombok.Data;
 @Data
 public class ServerWorkInfoVO {
 
-    @ApiModelProperty(value = "JVM中内存总大小")
-    private Long jvmTotalMemory;
+    @ApiModelProperty(value = "JVM中内存总大小（字节）")
+    private long jvmTotalMemory;
 
-    @ApiModelProperty(value = "JVM中内存剩余大小")
-    private Long jvmFreeMemory;
+    @ApiModelProperty(value = "JVM中内存剩余大小（字节）")
+    private long jvmFreeMemory;
 
-    @ApiModelProperty(value = "系统总内存")
-    private Long memoryTotal;
+    @ApiModelProperty(value = "系统总内存（字节）")
+    private long memoryTotal;
 
-    @ApiModelProperty(value = "系统可用内存")
-    private Long memoryAvailable;
+    @ApiModelProperty(value = "系统可用内存（字节）")
+    private long memoryAvailable;
 
     @ApiModelProperty(value = "CPU总的使用率")
     private double cpuTotal;
 
-    @ApiModelProperty(value = "磁盘总量")
-    private Long diskTotal;
+    @ApiModelProperty(value = "磁盘总量（字节）")
+    private long diskTotal;
 
-    @ApiModelProperty(value = "磁盘使用总量")
-    private Long diskUsing;
+    @ApiModelProperty(value = "磁盘使用总量（字节）")
+    private long diskUsing;
 
 }
