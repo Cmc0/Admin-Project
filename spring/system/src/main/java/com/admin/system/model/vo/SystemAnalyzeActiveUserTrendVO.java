f891305980e29afd4c135b17fa6a329e4bd51c8b
法外狂徒李四 <dimensional_logic@qq.com>
2022-07-08 21:15:49 +0800

    【v0.0.3】【update】

--- spring/system/src/main/java/com/admin/system/model/vo/SystemAnalyzeActiveUserTrendVO.java
+++ spring/system/src/main/java/com/admin/system/model/vo/SystemAnalyzeActiveUserTrendVO.java
@@ -9,7 +9,7 @@ public class SystemAnalyzeActiveUserTrendVO {
     @ApiModelProperty(value = "年月，格式：2022-07-08（字符串）")
     private String monthStr;
 
-    @ApiModelProperty(value = "月活跃人数")
+    @ApiModelProperty(value = "该月活跃人数")
     private long total;
 
 }
--- spring/system/src/main/java/com/admin/system/model/vo/SystemAnalyzeTrafficUsageVO.java
+++ spring/system/src/main/java/com/admin/system/model/vo/SystemAnalyzeTrafficUsageVO.java
@@ -1,7 +1,16 @@
 package com.admin.system.model.vo;
 
+import com.admin.common.model.enums.SysRequestCategoryEnum;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class SystemAnalyzeTrafficUsageVO {
+
+    @ApiModelProperty(value = "请求类别")
+    private SysRequestCategoryEnum category;
+
+    @ApiModelProperty(value = "该类别请求总数")
+    private long total;
+
 }
