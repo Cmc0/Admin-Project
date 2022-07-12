4dde26e4c3e2242f5b47f9a26ae65839015382bd
cmc6 <dimlt@vip.qq.com>
2022-07-12 15:23:39 +0800

    【v0.0.3】【update】

--- spring/start/src/test/java/com/admin/start/DoAdminPackage.java
+++ spring/start/src/test/java/com/admin/start/DoAdminPackage.java
@@ -1,6 +1,9 @@
 package com.admin.start;
 
+import cn.hutool.core.io.FileUtil;
 import lombok.SneakyThrows;
+
+import java.io.File;
 
 public class DoAdminPackage {
 
@@ -10,7 +13,17 @@ public class DoAdminPackage {
     @SneakyThrows
     public static void main(String[] args) {
 
-        System.out.println(System.getProperty("user.dir"));
+        String springPath = System.getProperty("user.dir"); // 例如：E:\Cmc0\Admin-Project\spring
+
+        //        RuntimeUtil.execForStr("cmd /c cd " + springPath, "cmd /c mvn clean");
+
+        File springFile = FileUtil.newFile(springPath);
+
+        //        RuntimeUtil.exec(null, springFile, "cmd mvn clean");
+
+        String jarPath = springPath + "/start/target/start-0.0.1-SNAPSHOT.jar";
+
+        File jarFile = FileUtil.newFile(jarPath);
 
         //        Session session = JschUtil.getSession("139.196.121.3", 22, "root", "E:\\demo\\key1.pem", null);
         //
