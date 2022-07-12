e043f2ce7c6fe2f457867091a8624e26d4afca25
cmc6 <dimlt@vip.qq.com>
2022-07-12 14:49:04 +0800

    【v0.0.3】【update】

---
+++ spring/start/src/test/java/com/admin/start/DoAdminPackage.java
@@ -0,0 +1,23 @@
+package com.admin.start;
+
+import lombok.SneakyThrows;
+
+public class DoAdminPackage {
+
+    /**
+     * 打包：前端和后端
+     */
+    @SneakyThrows
+    public static void main(String[] args) {
+
+        System.out.println(System.getProperty("user.dir"));
+
+        //        Session session = JschUtil.getSession("139.196.121.3", 22, "root", "E:\\demo\\key1.pem", null);
+        //
+        //        Sftp sftp = JschUtil.createSftp(session);
+        //
+        //        sftp.close();
+
+    }
+
+}
