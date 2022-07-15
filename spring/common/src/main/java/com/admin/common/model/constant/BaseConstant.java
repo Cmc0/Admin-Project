8aaf27bff11658f5f14af382aab3f284c8f47e30
法外狂徒李四 <dimensional_logic@qq.com>
2022-07-15 18:40:01 +0800

    【v0.0.3】【update】

--- spring/common/src/main/java/com/admin/common/model/constant/BaseConstant.java
+++ spring/common/src/main/java/com/admin/common/model/constant/BaseConstant.java
@@ -8,7 +8,7 @@ public interface BaseConstant {
     String NEGATIVE_ONE_STR = "-1";
 
     // mq 相关 ↓
-    String MQ_WEB_SOCKET_TOPIC = "cmc-admin-web-socket-topic"; // WebSocket的 topic
+    String MQ_WEB_SOCKET_TOPIC = "admin-project-web-socket-topic"; // webSocket的 topic
     // mq 相关 ↑
 
     // 过期时间相关 ↓
