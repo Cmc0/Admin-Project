afffd425550f508aa9acc8f57f361ecada45cd8a
cmc6 <dimlt@vip.qq.com>
2022-07-20 16:17:03 +0800

    【v0.0.3】【update】

--- spring/start/src/main/java/com/admin/start/StartApplication.java
+++ spring/start/src/main/java/com/admin/start/StartApplication.java
@@ -2,10 +2,8 @@ package com.admin.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc // 防止 swagger 启动报错
 public class StartApplication {
 
     public static void main(String[] args) {
