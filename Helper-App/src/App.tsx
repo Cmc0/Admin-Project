8c532698936e6ae3c735330f8e06cfa5c1d0d45a
cmc6 <dimlt@vip.qq.com>
2022-07-21 10:24:41 +0800

    【v0.0.3】【update】【移除 Helper-App】

--- Helper-App/src/App.tsx
+++
@@ -1,5 +0,0 @@
-import MyConvert from "@/page/MyConvert/MyConvert";
-
-export default function () {
-    return <MyConvert/>
-}
--- Helper-App/src/page/MyConvert/MyConvertHelper.ts
+++
@@ -1,7 +0,0 @@
-import {Dispatch, SetStateAction} from "react";
-
-// 方便写【方法】
-export default function (source: string, setResult: Dispatch<SetStateAction<string>>) {
-
-
-}
