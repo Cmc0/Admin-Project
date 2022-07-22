329011be1e7a6f022e0a0d328d5f21caf418f8a0
cmc6 <dimlt@vip.qq.com>
2022-07-22 11:05:14 +0800

    【v0.0.3】【update】

--- spring/im/src/main/java/com/admin/im/model/document/ImElasticsearchFriendRequestDocument.java
+++ spring/im/src/main/java/com/admin/im/model/document/ImElasticsearchFriendRequestDocument.java
@@ -1,15 +1,12 @@
 package com.admin.im.model.document;
 
-import com.admin.common.model.constant.BaseElasticsearchIndexConstant;
 import com.admin.im.model.enums.ImFriendRequestResultEnum;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import org.springframework.data.elasticsearch.annotations.Document;
 
 import java.util.Date;
 
 @Data
-@Document(indexName = BaseElasticsearchIndexConstant.IM_FRIEND_REQUEST_INDEX)
 public class ImElasticsearchFriendRequestDocument {
 
     @ApiModelProperty(value = "创建用户 id")
